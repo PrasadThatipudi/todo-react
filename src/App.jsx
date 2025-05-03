4515dc5d4e0d670d4e03bc31887a750fff4973ed
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-05-03 22:45:00 +0530

    Wires up Todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+class TaskItem extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  render() {
+    const textDecoration = this.props.done ? "line-through" : "none";
+
+    return (
+      <>
+        <div>
+          <p style={{ textDecoration }}>{this.props.description}</p>
+        </div>
+      </>
+    );
+  }
 }
 
-export default App
+class Tasks extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const tasks = this.props.tasks.map((task, index) => {
+      return (
+        <TaskItem
+          key={`${this.props.todoId}-${index}`}
+          description={task.description}
+          done={task.done}
+        ></TaskItem>
+      );
+    });
+
+    return <>{tasks}</>;
+  }
+}
+
+class Todo extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>{this.props.title}</h1>
+        <Tasks tasks={this.props.tasks} todoId={this.props.todoId}></Tasks>
+      </>
+    );
+  }
+}
+
+class Todos extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          title: "Shopping",
+          todoId: 0,
+          tasks: [{ description: "Item1", done: true }],
+        },
+      ],
+      nextTodoId: 1,
+    };
+  }
+
+  render() {
+    const todos = this.state.todos.map((todo) => {
+      return (
+        <Todo
+          key={todo.todoId}
+          tasks={todo.tasks}
+          todoId={todo.todoId}
+          title={todo.title}
+        ></Todo>
+      );
+    });
+
+    return <>{todos}</>;
+  }
+}
+
+export default Todos;
